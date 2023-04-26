5a1114d6d9c9e7e0dc251f10247949ea4988f83b
OmarKhalil10 <omar.khalil498@gmail.com>
2023-04-26 02:29:57 +0000

    edit task-3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@
 
 const request = require('request');
 const argv = process.argv;
-let url = 'https://swapi-api.alx-tools.com/api/films/:id';
+let url = 'https://swapi-api.alx-tools.com/api/films/';
 
 request(url + argv[2], function (err, res, body) {
   if (err) {
