53ab34aa0e0f1d212c640c1245e2baf4d43a6f28
OmarKhalil10 <omar.khalil498@gmail.com>
2023-04-26 02:35:31 +0000

    edit task-6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,22 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+  if (error) {
+    console.error(error);
   }
+  const dict = JSON.parse(body).reduce((acc, elem) => {
+    if (!acc[elem.userId]) {
+      if (elem.completed) {
+        acc[elem.userId] = 1;
+      }
+    } else {
+      if (elem.completed) {
+        acc[elem.userId] += 1;
+      }
+    }
+    return acc;
+  }, {});
+  console.log(dict);
 });
